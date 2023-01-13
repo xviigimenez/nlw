a9f1253d5be3b4038f0357fae5ff18a27d73a60d
xviigimenez <gimenez@tuta.io>
2023-01-13 16:34:48 -0300

    Realizada a etapa web da segunda aula

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
         './index.html',
     ],
   theme: {
-    extend: {},
+      extend: {
+          fontFamily: {
+              // Segunda fonte serve como reserva caso a primeira não funcione
+              sans: ['Inter', 'sans-serif']
+          },
+          backgroundImage: {
+              'galaxy': "url('/background-galaxy.png')",
+              'nlw-gradient': 'linear-gradient(90deg, rgba(93,169,182,1) 0%, rgba(180,195,66,1) 100%)',
+              'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.75) 75%)',
+          }
+          
+      },
   },
   plugins: [],
 }
